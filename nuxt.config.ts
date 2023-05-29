5b68e6ca0abcf1d6d1d70c7b7c87bd318c71f00a
PeterEhses <ehsesp@hochschule-trier.de>
2023-05-29 12:37:48 +0200

    /

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: true,
+  ssr: false,
   // nitro: {
   //   prerender: {
   //     crawlLinks: true
   //   }
   // }, 
+  content: {
+    experimental: {
+      clientDb: true
+    }
+  },
   app: {
     baseURL: '/IMDisplay/' // baseURL: '/<repository>/'
   },
