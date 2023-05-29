8cb2b4657bc3ce2499cc1272c317e53c051cec0e
PeterEhses <ehsesp@hochschule-trier.de>
2023-05-29 12:58:26 +0200

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  nitro: {
-    preset: 'service-worker'
-  },
+  // nitro: {
+  //   preset: 'service-worker'
+  // },
   // nitro: {
   //   prerender: {
   //     crawlLinks: true
