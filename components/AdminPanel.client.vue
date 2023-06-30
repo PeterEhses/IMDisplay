357506d2d81578f4f11004be7dbf896747641969
PeterEhses <ehsesp@hochschule-trier.de>
2023-06-30 23:16:46 +0200

    messy fix for cms login not working in prod

---
+++ components/AdminPanel.client.vue
@@ -0,0 +1,35 @@
+
+
+
+
+<template>
+  <div></div>
+</template>
+<script>
+import createReactClass from "create-react-class";
+import React from "react";
+
+import yaml from "js-yaml";
+window.CMS_MANUAL_INIT = true;
+import CMS, { init } from "netlify-cms-app";
+
+fetch("backend-config.yml", {
+  credentials: "same-origin",
+})
+  .then((response) => {
+    response.text().then((text) => {
+      const config = yaml.load(text);
+
+      config.load_config_file = false;
+      console.log(config);
+      init({ config: config });
+    //   window.CMS = CMS;
+    //   window.initCMS = CMS.init;
+    //   window.createClass = window.createClass || createReactClass;
+    //   window.h = window.h || React.createElement;
+    });
+  })
+  .catch((error) => error);
+
+export default {};
+</script>
--- pages/bdmin.vue
+++ pages/bdmin.vue
@@ -6,14 +6,17 @@ useHead({
 </script>
 
 <template>
-  <div></div>
+  <client-only>
+    <admin-panel />
+  </client-only>
 </template>
+
 <script>
-import CMS, { init } from "netlify-cms-app";
-if (window) {
-    window.CMS_MANUAL_INIT = true
-  const { CMS, initCMS: init } = window;
-  init();
-}
 export default {};
 </script>
+
+<style>
+#__nuxt{
+  display: none;
+}
+</style>
