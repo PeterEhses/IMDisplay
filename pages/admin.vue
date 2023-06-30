4675c7d087634aed5723996129239996c47f3f85
Peter Ehses <ehsesp@hochschule-trier.de>
2023-06-30 15:05:35 +0200

    htaccess wrangling

---
+++ pages/admin.vue
@@ -0,0 +1,19 @@
+<script setup>
+useHead({
+  meta: [{ name: "robots", content: "noindex" }],
+  title: "Content manager",
+});
+</script>
+
+<template>
+  <div></div>
+</template>
+<script>
+import CMS, { init } from "netlify-cms-app";
+if (window) {
+    window.CMS_MANUAL_INIT = true
+  const { CMS, initCMS: init } = window;
+  init();
+}
+export default {};
+</script>
